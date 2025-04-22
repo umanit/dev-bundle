a3ab6aa56ea7783ced3ae73cf2864c5e72f302b4
Alexandre Emion <aemion@umanit.fr>
2025-04-22 13:42:51 +0200

    feat: database resetter

---
+++ src/Foundry/CommandDatabaseResetter.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Umanit\DevBundle\Foundry;
+
+use DAMA\DoctrineTestBundle\Doctrine\DBAL\StaticDriver;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Zenstruck\Foundry\ORM\ResetDatabase\OrmResetter;
+
+final readonly class CommandDatabaseResetter implements OrmResetter
+{
+    private array $databases;
+
+    public function __construct(
+        private Registry $registry,
+        private string $resetCommand
+    ) {
+        $databases = [];
+        /** @var Connection $connection */
+        foreach ($this->registry->getConnections() as $name => $connection) {
+            // Nécessaire pour éviter d'interférer avec les transactions
+            StaticDriver::setKeepStaticConnections(false);
+            try {
+                /** @var AbstractPlatform $platform */
+                $platform = $connection->getDatabasePlatform();
+                $databases[$name] = $platform->quoteStringLiteral($connection->getDatabase());
+                $connection->close();
+            } catch (\Exception) {
+                // Si on n'arrive pas à récupérer le nom de la base de donnée de manière officielle,
+                // On se rabat sur le fonctionnement interne de doctrine (moins stable)
+                $databases[$name] = \sprintf("\'%s\'", $connection->getParams()['dbname']);
+            }
+            StaticDriver::setKeepStaticConnections(true);
+        }
+
+        $this->databases = $databases;
+    }
+
+    public function resetBeforeFirstTest(KernelInterface $kernel): void
+    {
+        // Nécessaire pour éviter d'interférer avec les transactions
+        StaticDriver::setKeepStaticConnections(false);
+        $this->resetDatabase($this->getApplication($kernel));
+        StaticDriver::setKeepStaticConnections(true);
+    }
+
+    private function resetDatabase(Application $application): void
+    {
+        $this->dropConnections($application);
+        $this->run($application, $this->resetCommand);
+    }
+
+    public function dropConnections(Application $application): void
+    {
+        $databases = array_map(static fn(string $name) => \sprintf("\'%s\'", $name), $this->databases);
+        $databases = implode(', ', $databases);
+
+        $sql = \sprintf(
+            'SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname IN (%s) AND pid <> pg_backend_pid()',
+            $databases
+        );
+        $this->run($application, \sprintf("dbal:run-sql '%s'", $sql), true);
+    }
+
+    private function run(Application $application, string $command, bool $allowFailure = false): int
+    {
+        $output = new BufferedOutput();
+        $exitCode = $application->run(new StringInput($command), $output);
+        if (!$allowFailure && $exitCode !== Command::SUCCESS) {
+            throw new \RuntimeException(\sprintf('Error running %s: %s', $command, $output->fetch()));
+        }
+
+        return $exitCode;
+    }
+
+    private function getApplication(KernelInterface $kernel): Application
+    {
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        return $application;
+    }
+
+    public function resetBeforeEachTest(KernelInterface $kernel): void
+    {
+    }
+}
--- src/UmanitDevBundle.php
+++ src/UmanitDevBundle.php
@@ -4,9 +4,44 @@ declare(strict_types=1);
 
 namespace Umanit\DevBundle;
 
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Umanit\DevBundle\Foundry\CommandDatabaseResetter;
+use Zenstruck\Foundry\ORM\ResetDatabase\OrmResetter;
 
 final class UmanitDevBundle extends AbstractBundle
 {
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        // @formatter:off
+        $definition
+            ->rootNode()
+            ->children()
+                ->scalarNode('database_resetter_command')
+                    ->info(
+                        <<<TXT
+Une commande symfony à exécuter pour le ResetDatabase de foundry. Laisser null pour ne pas utiliser le resetter
+TXT
+                    )
+                    ->defaultNull()
+                ->end()
+            ->end()
+        ;
+        // @formatter:on
+    }
 
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $command = $config['database_resetter_command'];
+        if ($command !== null) {
+            $builder
+                ->register('umanit_dev.orm_resetter', CommandDatabaseResetter::class)
+                ->setDecoratedService(OrmResetter::class)
+                ->setArguments([new Reference('doctrine'), $command])
+            ;
+        }
+    }
 }
