f4a28286d262d6f8cab06f0366407854b5fb5ad0
Alexandre Emion <aemion@umanit.fr>
2025-04-22 11:46:09 +0200

    feat: initial commit

---
+++ src/UmanitDevBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Umanit\DevBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+final class UmanitDevBundle extends AbstractBundle
+{
+
+}
